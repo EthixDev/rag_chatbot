7b55a4105937d2901da815c6af10d35e2c140fc8
merronmuche <merronmuche@gmail.com>
2024-12-25 10:41:19 +0300

    add app urls .py file

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import  path
+
+from app.views import generate_response,topic_view
+ 
+urlpatterns = [
+    # path('', generate_prompt, name='generate'),
+    path('', generate_response, name='generate_response'),
+    path('<int:id>/', topic_view, name='topic_view'),
+
+] 
