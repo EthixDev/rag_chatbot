cf11ef55a9aa586a19ab0ed05885f453976a427d
juniorworku <juniorworku@gmail.com>
2024-12-25 10:59:23 +0300

    Fixed embedding model deprecation

--- RAG/embedding_gen.py
+++ RAG/embedding_gen.py
@@ -5,12 +5,12 @@ from .read_chunking import read_docx, chunk_text
 load_dotenv()
 
 def embed_text(chunks):
-    embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
     embeds = embedding_model.embed_documents(chunks)
     return embeds
 
 if __name__ == "__main__":
-    doc_path = "/path/to/document.docx"
+    doc_path = "C:/Users/haileyesus/Desktop/Ethix/rag_chatbot/docs/Edge.docx"
     
     doc_text = read_docx(doc_path)
     chunks = chunk_text(doc_text,chunk_size=1500, chunk_overlap=5)
