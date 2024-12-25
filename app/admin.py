b040dadf8842184d4c26ef4c3afd834a8728e3ae
merronmuche <merronmuche@gmail.com>
2024-12-25 22:52:57 +0300

    Removed embbeding in the admin

--- app/admin.py
+++ app/admin.py
@@ -8,7 +8,7 @@ class DocumentAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
 
 class TextChunkAdmin(admin.ModelAdmin):
-    list_display = ('chunk', 'embedding', 'updated_at')
+    list_display = ('chunk', 'updated_at')
   
 
 class ConversationAdmin(admin.ModelAdmin):
