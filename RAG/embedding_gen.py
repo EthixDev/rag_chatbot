7a413bc15e3b220869c4f7ccd734357d8dba7491
merronmuche <merronmuche@gmail.com>
2024-12-25 23:05:46 +0300

    Refactor the code

--- RAG/embedding_gen.py
+++ RAG/embedding_gen.py
@@ -14,10 +14,10 @@ def embed_text(chunk):
         model = 'models/text-embedding-004',
         content=chunk,
     )
-    return embeddings
+    return embeddings["embedding"]  
 
 if __name__ == "__main__":
-    doc_path = "/home/meron/Documents/ethix/codes/rag_chatbot/Edge.docx"
+    doc_path = "/path/to/document"
     
     doc_text = read_docx(doc_path)
     chunks = chunk_text(doc_text,chunk_size=1500, chunk_overlap=5)
