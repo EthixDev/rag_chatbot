2802f0f579c5659c4113f325b8ec0fb49ab73aea
juniorworku <juniorworku@gmail.com>
2024-12-25 10:59:59 +0300

    Refactoring teh code

--- RAG/read_chunking.py
+++ RAG/read_chunking.py
@@ -10,12 +10,13 @@ def read_docx(file_path):
     
     return text
 
-def chunk_text(doc, chunk_size=2000, chunk_overlap=5):
+def chunk_text(doc, chunk_size=500, chunk_overlap=5):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_text(doc)
     return chunks
 
 if __name__ == "__main__":
-    file_path = "/path/to/document.docx"
+    file_path = "C:/Users/haileyesus/Desktop/Ethix/rag_chatbot/docs/Edge.docx"
     doc_text = read_docx(file_path)
+    chunk_doc = chunk_text(doc_text)
     print(doc_text)
