68d9ebccfded046c6d47afa53cdec0f28f1f896f
merronmuche <merronmuche@gmail.com>
2024-12-30 17:33:02 +0300

    refactor

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,8 @@
-from django.urls import  path
+from django.urls import path
+from app.views import generate_response, topic_view, index
 
-from app.views import generate_response,topic_view, index
- 
 urlpatterns = [
-    # path('', generate_prompt, name='generate'),
-    path('chat', generate_response, name='generate_response'),
+    path('', generate_response, name='generate_response'),  # Root path for the app
     path('<int:id>/', topic_view, name='topic_view'),
-    path('chunk', index, name='index')
-
-] 
+    path('chunk', index, name='index'),
+]
