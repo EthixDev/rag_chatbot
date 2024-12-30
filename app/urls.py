f1799272ecdf9a634ad357fff4892b0a656379ae
merronmuche <merronmuche@gmail.com>
2024-12-30 17:04:59 +0300

    add html code to ui

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import  path
 
-from app.views import generate_response,topic_view
+from app.views import generate_response,topic_view, index
  
 urlpatterns = [
     # path('', generate_prompt, name='generate'),
-    path('', generate_response, name='generate_response'),
+    path('chat', generate_response, name='generate_response'),
     path('<int:id>/', topic_view, name='topic_view'),
+    path('chunk', index, name='index')
 
 ] 
