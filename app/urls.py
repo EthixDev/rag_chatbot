5323951291324e2546a8b274b1a53316e254f9ef
merronmuche <merronmuche@gmail.com>
2025-01-02 18:22:34 +0300

    add redirecting button to similar chunk

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app.views import generate_response, topic_view, index, DocumentUpload, dele
 urlpatterns = [
     path('', generate_response, name='generate_response'),  # Root path for the app
     path('<int:id>/', topic_view, name='topic_view'),
-    path('chunk', index, name='index'),
+    path('chunk/', index, name='index'),
     path('api/upload_document/', DocumentUpload.as_view(), name='api_upload_document'),
     path('delete-topic/', delete_topic, name='delete_topic'),
 
